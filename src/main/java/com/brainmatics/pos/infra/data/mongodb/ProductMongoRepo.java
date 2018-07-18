70ef0666ffb7b21ed84f468ed6a3d20acdfb55be
Studen - 11 <brainpos>
2018-07-18 10:43:19 +0700

    monggodbAAAA

--- src/main/java/com/brainmatics/pos/infra/data/mongodb/ProductMongoRepo.java
+++ src/main/java/com/brainmatics/pos/infra/data/mongodb/ProductMongoRepo.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 public class ProductMongoRepo implements ProductRepo {
 
     public int getCount() {
-        System.out.println("from mongodb");
+        System.out.println("from mongodbAAAA");
         return 1;
     }
 
     public Product getById(int id) {
-        System.out.println("from mongodb");
+        System.out.println("from mongodbAAAA");
         return new Product();
     }
 
